01fa4a4fd1b46ce823b98eeae31a4c7d5efb3cec
weips <weips001@163.com>
2021-03-12 06:41:15 +0800

    定义user骨架

--- app/model/user.js
+++ app/model/user.js
@@ -6,6 +6,7 @@ module.exports = app => {
 
   const UserSchema = new Schema({
     id: Schema.Types.ObjectId,
+    userCode: String,
     userName: {
       type: String,
       required: true
@@ -19,6 +20,16 @@ module.exports = app => {
     createTime: { 
       type: String,
       default: new Date().toString()
+    },
+    status: {
+      // 0 - 在职 1 - 离职
+      type: String,
+      default: '0'
+    },
+    // 不应返回在数据中
+    password: {
+      // 密码应该使用某种加密方式进行储存
+      type: String
     },
     compId: {
       type: Schema.Types.ObjectId,
