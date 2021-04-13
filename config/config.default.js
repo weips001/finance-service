6c5da35176e2a97dfc63394436f5a9303e6d72d5
weips <weips001@163.com>
2021-04-13 23:28:16 +0800

    增加mongoose 启动配置

--- config/config.default.js
+++ config/config.default.js
@@ -47,6 +47,7 @@ module.exports = appInfo => {
       url: 'mongodb://127.0.0.1:27017/test',
       options: {
         useNewUrlParser: true,
+        useUnifiedTopology:true
       },
     },
   };
